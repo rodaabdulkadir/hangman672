526597c0e8995ca801cd1d0467703a79310eab81
rodaabdulkadir <129794377+rodaabdulkadir@users.noreply.github.com>
2023-12-12 21:15:39 +0000

    Update milestone_4.py

--- milestone_4.py
+++ milestone_4.py
@@ -2,50 +2,58 @@ import random
 
 class Hangman:
     def __init__(self, word_list, num_lives=5):
-        self.word_list = word_list
-        self.num_lives = num_lives
-        self.list_of_guesses = []
-        self.word = random.choice(self.word_list)
-        self.word_guessed = ['_' for _ in self.word]
-        self.num_letters = len(set(self.word))
+        self._word_list = word_list
+        self._num_lives = num_lives
+        self._list_of_guesses = []
+        self._word = random.choice(self._word_list)
+        self._word_guessed = ['_' for _ in self._word]
+        self._num_letters = len(set(self._word))
 
-    def display_word(self):
-        return ' '.join(self.word_guessed)
+    def _update_word_guessed(self, guess):
+        for i, letter in enumerate(self._word):
+            if letter == guess:
+                self._word_guessed[i] = guess
+        self._num_letters -= 1
 
-    def display_guesses(self):
-        return ', '.join(self.list_of_guesses)
+    def _handle_correct_guess(self, guess):
+        print(f"Good guess! '{guess}' is in the word.")
+        self._update_word_guessed(guess)
+
+    def _handle_incorrect_guess(self, guess):
+        self._num_lives -= 1
+        print(f"Sorry, '{guess}' is not in the word.")
+        print(f"You have {self._num_lives} lives left.")
 
     def check_guess(self, guess):
-        guess = guess.lower()  # Convert guessed letter to lowercase
-        if guess in self.word:
-            print(f"Good guess! '{guess}' is in the word.")
-            for i, letter in enumerate(self.word):
-                if letter == guess:
-                    self.word_guessed[i] = guess  # Replace '_' in word_guessed with guess
-            self.num_letters -= 1  # Reduce num_letters by 1 after correct guess
+        guess = guess.lower()
+        if guess in self._word:
+            self._handle_correct_guess(guess)
         else:
-            self.num_lives -= 1  # Step 2: Reduce num_lives by 1 if guess is incorrect
-            print(f"Sorry, '{guess}' is not in the word.")
-            print(f"You have {self.num_lives} lives left.")
+            self._handle_incorrect_guess(guess)
 
     def ask_for_input(self):
-        while True:  # Create a while loop
+        while True:
             guess = input("Guess a letter: ")
             if len(guess) != 1 or not guess.isalpha():
                 print("Invalid letter. Please enter a single alphabetical character.")
-            elif guess in self.list_of_guesses:
+            elif guess in self._list_of_guesses:
                 print("You already tried that letter!")
             else:
-                self.check_guess(guess)  # Call the check_guess method
-                self.list_of_guesses.append(guess)  # Append guess to list_of_guesses
-                break  # Exit loop after successful guess
+                self.check_guess(guess)
+                self._list_of_guesses.append(guess)
+                break
+
+    def display_word(self):
+        return ' '.join(self._word_guessed)
+
+    def display_guesses(self):
+        return ', '.join(self._list_of_guesses)
 
 def main():
-    # Example usage of the Hangman class
     words = ['apple', 'banana', 'orange', 'grape', 'kiwi']
     game = Hangman(words)
 
-    game.ask_for_input()  # Call the ask_for_input method
+    game.ask_for_input()
     print("Word after guess:", game.display_word())
 
 if __name__ == "__main__":
