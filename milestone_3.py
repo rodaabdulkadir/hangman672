cd1bca5227f218007f1075ab45828a0b881600bd
rodaabdulkadir <129794377+rodaabdulkadir@users.noreply.github.com>
2023-12-12 21:10:16 +0000

    Update milestone_3.py

--- milestone_3.py
+++ milestone_3.py
@@ -1,26 +1,37 @@
 import random
 
-def check_guess(guess, secret_word):
-    guess = guess.lower()  # Step 2: Convert the guess to lower case
+def check_guess_in_word(guess, secret_word):
+    # Checks if the guessed letter is in the secret word
+    guess = guess.lower()
     if guess in secret_word:
         print(f"Good guess! '{guess}' is in the word.")
+        return True
     else:
         print(f"Sorry, '{guess}' is not in the word. Try again.")
+        return False
 
-def ask_for_input(secret_word):
+def get_valid_letter():
+    # Function to get a valid letter guess from the user
     while True:
         guess = input("Guess a letter: ")
         if len(guess) == 1 and guess.isalpha():
-            check_guess(guess, secret_word)  # Step 3: Call check_guess function to check the guess
-            break
+            return guess
         else:
             print("Invalid letter. Please enter a single alphabetical character.")
+
+def play_game(secret_word):
+    # Function to initiate the game
+    while True:
+        user_guess = get_valid_letter()
+        if check_guess_in_word(user_guess, secret_word):
+            break
 
 def main():
     secret_words = ['apple', 'banana', 'orange', 'grape', 'kiwi']
     secret_word = random.choice(secret_words)
 
-    ask_for_input(secret_word)  # Step 4: Call ask_for_input function
+    play_game(secret_word)
 
 if __name__ == "__main__":
     main()
+
