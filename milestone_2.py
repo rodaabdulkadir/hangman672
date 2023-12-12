99efd31d97e517d8c62b7f4bc01bfa597846baeb
rodaabdulkadir <129794377+rodaabdulkadir@users.noreply.github.com>
2023-12-12 20:53:59 +0000

    Update milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -1,8 +1,27 @@
-# Step 1: Create a list of 5 favorite fruits
+import random  # Step 2: Importing the random module
+
+# Step 1: List of 5 favorite fruits
 favorite_fruits = ['Apple', 'Banana', 'Strawberry', 'Mango', 'Pineapple']
 
-# Step 2: Assign the list to a variable called word_list
+# Step 3: Selecting a random word from the word_list
 word_list = favorite_fruits
+word = random.choice(word_list)  # Step 4: Assigning the randomly generated word to a variable called 'word'
 
-# Step 3: Print the newly created list
-print(word_list)
+# Step 5: Print out the randomly chosen word
+print(word)
+
+#Task 3
+# Step 1: Ask the user to enter a single letter
+guess = input("Please enter a single letter: ")  # Step 2: Assign the input to a variable called 'guess'
+
+# Display the user's input
+print("You entered:", guess)
+
+Task 4
+# Step 1: Validation using if-else conditions
+if len(guess) == 1 and guess.isalpha():
+    # Step 2: Print "Good guess!" if the conditions are met
+    print("Good guess!")
+else:
+    # Step 3: Print an error message if the conditions are not met
+    print("Oops! That is not a valid input.")
