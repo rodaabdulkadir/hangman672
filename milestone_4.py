700dba06df32b1a09d09ed8eba24111a1f44dcc8
rodaabdulkadir <129794377+rodaabdulkadir@users.noreply.github.com>
2023-12-12 21:13:19 +0000

    Update milestone_4.py

--- milestone_4.py
+++ milestone_4.py
@@ -15,15 +15,31 @@ class Hangman:
     def display_guesses(self):
         return ', '.join(self.list_of_guesses)
 
+    def check_guess(self, guess):
+        guess = guess.lower()  # Step 1: Convert guessed letter to lowercase
+        if guess in self.word:
+            print(f"Good guess! '{guess}' is in the word.")
+        # Continue with the logic for the check_guess method in the next task
+
+    def ask_for_input(self):
+        while True:  # Step 2: Create a while loop
+            guess = input("Guess a letter: ")
+            if len(guess) != 1 or not guess.isalpha():
+                print("Invalid letter. Please enter a single alphabetical character.")
+            elif guess in self.list_of_guesses:
+                print("You already tried that letter!")
+            else:
+                self.check_guess(guess)  # Call the check_guess method
+                self.list_of_guesses.append(guess)  # Append guess to list_of_guesses
+                break  # Exit loop after successful guess
+
 def main():
     # Example usage of the Hangman class
     words = ['apple', 'banana', 'orange', 'grape', 'kiwi']
     game = Hangman(words)
 
-    print("Word to guess:", game.display_word())
-    print("Number of unique letters:", game.num_letters)
-    print("Number of lives:", game.num_lives)
-    print("List of guesses:", game.display_guesses())
+    game.ask_for_input()  # Step 3: Call the ask_for_input method
 
 if __name__ == "__main__":
     main()
+
