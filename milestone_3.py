597fb6b7e806a3e1a72c9921be43c594665120b0
rodaabdulkadir <129794377+rodaabdulkadir@users.noreply.github.com>
2023-12-12 21:09:18 +0000

    Update milestone_3.py

--- milestone_3.py
+++ milestone_3.py
@@ -1,30 +1,26 @@
 import random
 
-def get_valid_letter():
-    while True:
-        guess = input("Guess a letter: ")
-        if len(guess) == 1 and guess.isalpha():
-            return guess
-        else:
-            print("Invalid letter. Please enter a single alphabetical character.")
-
-def check_guess_in_word(guess, secret_word):
+def check_guess(guess, secret_word):
+    guess = guess.lower()  # Step 2: Convert the guess to lower case
     if guess in secret_word:
         print(f"Good guess! '{guess}' is in the word.")
     else:
         print(f"Sorry, '{guess}' is not in the word. Try again.")
 
+def ask_for_input(secret_word):
+    while True:
+        guess = input("Guess a letter: ")
+        if len(guess) == 1 and guess.isalpha():
+            check_guess(guess, secret_word)  # Step 3: Call check_guess function to check the guess
+            break
+        else:
+            print("Invalid letter. Please enter a single alphabetical character.")
+
 def main():
-    # List of secret words (you can replace this with your own list)
     secret_words = ['apple', 'banana', 'orange', 'grape', 'kiwi']
-    
-    # Choose a random word from the list
     secret_word = random.choice(secret_words)
 
-    while True:
-        user_guess = get_valid_letter()
-        check_guess_in_word(user_guess, secret_word)
-        break  # For demonstration purposes; replace this with your intended logic
+    ask_for_input(secret_word)  # Step 4: Call ask_for_input function
 
 if __name__ == "__main__":
     main()
